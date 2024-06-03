d67f2dd366f12ebd5bc180965ac00dbb96619379
MateusPy <mateusorlando2014@gmail.com>
2024-06-03 14:28:51 -0300

    Criação do app.get dos dados da tabela ESTTISTICAS_CARRINHO
    Co-authored-by: Artur <artur190503@icloud.com>

--- app/api/src/index.js
+++ app/api/src/index.js
@@ -1,19 +1,30 @@
 const express = require('express')
 
-//const {Pool} = require('pg')
+const {Pool} = require('pg')
 
-require('dotenv').config()
 
 const PORT = 3333
 
-/*const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL
-})*/
+const pool = new Pool({
+    user: 'alunos',
+    host: 'localhost',
+    database: 'ProjetoIntegrador1',
+    password: 'alunospi1',
+    port: 5432,
+  });
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {console.log('teste')})
+app.get('/dadosCarrinho', async (req, res) => {
+    try {
+      const result = await pool.query('SELECT * FROM ESTATISTICAS_CARRINHO');
+      res.json(result.rows);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
